3e0a98ba8ccc83e39375232274e63fe3cded5c1f
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-08-29 18:46:48 +0530

    Rewrote credential generator method

--- src/com/oopslab/services/CredentialGeneratorService.java
+++ src/com/oopslab/services/CredentialGeneratorService.java
@@ -21,14 +21,25 @@ public class CredentialGeneratorService {
         char[] password = new char[8];
         Random rand = new Random();
 
-        for (int i = 0; i < 2; i++) {
-            password[0 + 4 * i] = upperCaseLetters.charAt(rand.nextInt(upperCaseLetters.length()));
-            password[1 + 4 * i] = lowerCaseLetters.charAt(rand.nextInt(lowerCaseLetters.length()));
-            password[2 + 4 * i] = numbers.charAt(rand.nextInt(numbers.length()));
-            password[3 + 4 * i] = specialCharacters.charAt(rand.nextInt(specialCharacters.length()));
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (i == 0) {
+                    password[i + j] = upperCaseLetters.charAt(rand.nextInt(upperCaseLetters.length()));
+                    continue;
+                } else if (i == 1) {
+                    password[i + j + 1] = lowerCaseLetters.charAt(rand.nextInt(lowerCaseLetters.length()));
+                    continue;
+                } else if (i == 2) {
+                    password[i + j + 2] = numbers.charAt(rand.nextInt(numbers.length()));
+                    continue;
+                } else {
+                    password[i + j + 3] = specialCharacters.charAt(rand.nextInt(specialCharacters.length()));
+                    continue;
+                }
+            }
         }
 
-        return password.toString();
+        return (String.valueOf(password));
 
     }
 }
